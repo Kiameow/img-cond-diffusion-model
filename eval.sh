ea9281c0d559f93f2715356045423d2209831198
Kiameow <zjzootl@qq.com>
2025-02-07 16:05:49 +0800

    multi card train

--- eval.sh
+++ eval.sh
@@ -1,2 +1,4 @@
 export PYTHONPATH=/home/kia/Codes/img-cond-diffusion-model/
-python UPD_study/models/ours/ours_trainer.py --fold=0 -ev=t --modality=OPMED -dw=True --image_size=256 --normalize=True --h_config=UPD_study/models/ours/omega_config_big_eval_split.yaml
+accelerate launch \
+            --num_processes=4 --mixed_precision=fp16 \
+            UPD_study/models/ours/ours_trainer.py --fold=0 -ev=t --modality=OPMED -dw=True --image_size=256 --normalize=True --h_config=UPD_study/models/ours/omega_config_big_eval_split.yaml
--- train.sh
+++ train.sh
@@ -1,2 +1,4 @@
 export PYTHONPATH=/home/kia/Codes/img-cond-diffusion-model/
-python UPD_study/models/ours/ours_trainer.py --fold=0 --modality=OPMED -dw=True --image_size=256 --normalize=True
+accelerate launch \
+            --num_processes=4 --mixed_precision=fp16 \
+            UPD_study/models/ours/ours_trainer.py --fold=0 --modality=OPMED -dw=True --image_size=256 --normalize=True
