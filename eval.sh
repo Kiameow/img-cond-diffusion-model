11b6b93dc221934c46618f3eb0ad9abfd53f394e
Kiameow <zjzootl@qq.com>
2025-02-07 16:00:12 +0800

    update eval.sh

--- eval.sh
+++ eval.sh
@@ -1,2 +1,2 @@
 export PYTHONPATH=/home/kia/Codes/img-cond-diffusion-model/
-python UPD_study/models/ours/ours_trainer.py --fold=0 -ev=t --modality=OPMED -dw=True --image_size=256 --normalize=True
+python UPD_study/models/ours/ours_trainer.py --fold=0 -ev=t --modality=OPMED -dw=True --image_size=256 --normalize=True --h_config=UPD_study/models/ours/omega_config_big_eval_split.yaml
